096c48a05196013eedb859ed7b080f46d636472a
Бунчук Денис <>
2023-08-25 16:09:16 +0300

    Hometask2

---
+++ hometask2.js
@@ -0,0 +1,8 @@
+let FirstVariable = "We are good"
+//console.log (FirstVariable)
+let SecondVariable = "Hello"
+let ThirdVariable = "World"
+let Phrase = `${SecondVariable} ${ThirdVariable}`
+console.log(Phrase)
+console.log(typeof Phrase)
+// ( `hello ${"name"}` ) NOT CORRECT -> `hello ${name}
